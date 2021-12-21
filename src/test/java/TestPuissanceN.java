891fe3d14d282c3598bd331884b4054398beffc5
MasterFiyeur <theo5julien@outlook.fr>
2021-12-22 00:09:31 +0100

    Test about playing function

--- src/test/java/TestPuissanceN.java
+++ src/test/java/TestPuissanceN.java
@@ -1,11 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Scanner;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Service;
 
 import fourinarow.classes.PuissanceN;
 import fourinarow.classes.PuissanceN.InvalidSizeException;
+import fourinarow.classes.Tictactoe.Player;
 
 @Service
 class TestPuissanceN {
@@ -31,6 +34,49 @@ class TestPuissanceN {
 		Assert.assertEquals(6,game.getHeight());
 		Assert.assertEquals(7,game.getGrid().length);
 		Assert.assertEquals(6,game.getGrid()[0].length);
+	}
+	
+	@Test 
+	void playNegativeX() throws InvalidSizeException{
+		PuissanceN game = new PuissanceN(4);
+		game.play(-1,Player.PLAYER);
+		Assert.assertEquals("The index must be between 0 and "+(game.getWidth()-1),game.getMessage());
+	}
+	
+	@Test 
+	void playOutLimitX() throws InvalidSizeException{
+		PuissanceN game = new PuissanceN(4);
+		game.play(7,Player.PLAYER);
+		Assert.assertEquals("The index must be between 0 and "+(game.getWidth()-1),game.getMessage());
+	}
+	
+	@Test 
+	void playFullColumn() throws InvalidSizeException{
+		PuissanceN game = new PuissanceN(4);
+		for(int i=0;i<=game.getHeight();i++) {
+			game.play(0,Player.PLAYER);
+		}
+		Assert.assertEquals("The column is full, you can't play there",game.getMessage());
+	}
+	
+	@Test
+	void playEverywhere() throws InvalidSizeException{
+		PuissanceN game = new PuissanceN(4);
+		Player player;
+		int compt = 0;
+		for (int i = 0; i < 7;i++) {
+			compt = 0;
+			if(i%2==0) {player = Player.PLAYER;}else{player = Player.IA;}
+			for (int j = 0;j<game.getHeight();j++) {
+				game.play(i,player);
+				compt++;
+				if(compt == 2) {
+					compt = 0;
+					player = (player==Player.PLAYER)?Player.IA:Player.PLAYER;
+				}
+				assertEquals("ok",game.getMessage());
+			}
+		}
 	}
 }
 
