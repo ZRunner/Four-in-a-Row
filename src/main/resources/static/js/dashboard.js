99ec1842b80d2df9e4a42df6c1196d6743dc484f
ZRunner <z.runner.mc@gmail.com>
2021-12-22 09:39:21 +0100

    improve account creation date display

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -7,17 +7,8 @@ function setInfos(){
         url: '/api/me/profile',
         success: function(data){
         	let creationDate = new Date(data.createdAt);
-        	let dd = creationDate.getDate();
-            let mm = creationDate.getMonth() + 1;
-            let yyyy = creationDate.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd;
-            }
-            if (mm < 10) {
-                mm = '0' + mm;
-            }
         	username.innerHTML = data.username;
-        	date_creation.innerHTML = dd + '/' + mm + '/' + yyyy;
+        	date_creation.innerHTML = creationDate.toLocaleDateString();
         },
         error: function(error){
         	$(".info-message").text(error.responseText);
